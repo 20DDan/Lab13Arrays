b195361b366cc2ad131241f8ff15634e9d9f7dff
Dan Ratiu <dan.ratiu85@gmail.com>
2022-04-26 19:43:43 +0300

    Check Bigger Number Lab 11 Ex 3 update

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
 
         System.out.println("Lab 11 Exercise 3 Check Bigger Number");
         LogicalOperations op = new LogicalOperations();
-        double grandest = op.checkBiggerNumber(5,9000);
-        float massive = op.checkBiggerNumber(10001,-900000);
-        int biggest = op.checkBiggerNumber(56,51);
+        double grandest = op.checkBiggerNumber(5, 9000);
+        float massive = op.checkBiggerNumber(10001, -900000);
+        int biggest = op.checkBiggerNumber(56, 51);
         System.out.println(biggest);
         System.out.println(massive);
         System.out.println(grandest);
@@ -18,10 +18,10 @@ public class Main {
         String Ionel;
         String Ghita;
 
+
         System.out.println(" Mihai &" + " Ionel &" + " Gusti" + " toti au mers la piata");
 
         Calculator myCalculator = new Calculator();
-
 
 
         double A = 57;
@@ -74,6 +74,16 @@ public class Main {
         System.out.println("Exercise 4 - This shows 3 numbers added and average of them");
         System.out.println(averageNumbers(a, b, c));
     }
+
+    public static String textComparison(String text) {
+        if (text.equals("FastTrackIT")) {
+            return "Learning text comparison";
+
+        } else
+
+            return "Got to try some more";
+    }
+
 
     public static double averageNumbers(double nr1, double nr2, double nr3) {
         double result = (nr1 + nr2 + nr3) / 3;
