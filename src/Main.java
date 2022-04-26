474bb4e421626d7c4fcf4b944eea60d7fae98056
Dan Ratiu <dan.ratiu85@gmail.com>
2022-04-26 18:43:32 +0300

    MyCalculator Update

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ public class Main {
 
         System.out.println(" Mihai &" + " Ionel &" + " Gusti" + " toti au mers la piata");
 
+        Calculator myCalculator = new Calculator();
+
         double A = 57;
         double B = 4;
 
@@ -22,22 +24,22 @@ public class Main {
         System.out.println("Part A Method 1 Directly Displayed");
         System.out.println(-5 + 8 * 6);
         System.out.println("Part A Method 2 using Sum");
-        System.out.println(sum(-5, 8 * 6));
+        System.out.println(myCalculator.add(-5, 8 * 6));
         System.out.println("Part B");
-        System.out.println(sum(55, 9 / 9f));
+        System.out.println(myCalculator.add(55, 9 / 9f));
         System.out.println("Part C");
-        System.out.println(sum(20, -3 * 5f / 8));
+        System.out.println(myCalculator.add(20, -3 * 5f / 8));
         System.out.println("Part D");
-        System.out.println(sum(5, 15 / 3f * 2 - 8f / 3));
+        System.out.println(myCalculator.add(5, 15 / 3f * 2 - 8f / 3));
 
         System.out.println("Result from adding 2 numbers together");
-        System.out.println(sum(57, 7));
+        System.out.println(myCalculator.add(57, 7));
         System.out.println("Result from subtracting 2 numbers together");
-        System.out.println(subtract(695, 7));
+        System.out.println(myCalculator.subtract(695, 7));
         System.out.println("Result from multiplying 2 numbers together");
-        System.out.println(multiply(467, 567));
+        System.out.println(myCalculator.multiply(467, 567));
         System.out.println("Result from dividing 2 numbers together");
-        System.out.println(divide(456, 45));
+        System.out.println(myCalculator.divide(456, 45));
 
         System.out.println("This is Exercise 3");
         printJava();
@@ -49,9 +51,9 @@ public class Main {
         System.out.println(convertTemp(70));
 
         System.out.println("Exercise 6 - This is the result of remainder of two numbers");
-        System.out.println(moduloRest(300, 549));
+        System.out.println(myCalculator.moduloRest(300, 549));
 
-        conversionHell(2004,24,7,1);
+        conversionHell(2004, 24, 7, 1);
 
         double a = 5006;
         double b = 795;
@@ -66,10 +68,6 @@ public class Main {
         return result;
     }
 
-    public static double moduloRest(double x, double y) {
-        double result = x % y;
-        return result;
-    }
 
     public static double convertImperialToMetric(double imperialLength) {
         double centimeter = imperialLength * 2.54;
@@ -81,7 +79,6 @@ public class Main {
         double celcius = (5 / 9f * (fahrenheit - 32));
         return celcius;
     }
-
 
 
     public static void printJava() {
@@ -96,41 +93,20 @@ public class Main {
             double distanceInMeters, double timeSecond, double timeMinute, double timeHour) {
         double totalTimeInSeconds = (timeHour * 60 * 60) + (timeMinute * 60) + timeSecond;
         double metersPerSecond = distanceInMeters * totalTimeInSeconds;
+
         System.out.println("Exercise 9 showing Meters Per Second");
         System.out.println(metersPerSecond);
         double totalTimeInHours = timeHour + timeSecond / 60 / 60 + timeMinute / 60;
         double distanceInKilometers = distanceInMeters / 1000;
         double kilometersPerHour = totalTimeInHours * distanceInKilometers;
+
         System.out.println("Exercise 9 showing Kilometers per hour");
         System.out.println(kilometersPerHour);
         double distanceInMiles = distanceInKilometers * 1.609;
         double milesPerHour = distanceInMiles * totalTimeInHours;
+
         System.out.println("Exercise 9 Showing Miles Per Hour");
         System.out.println(milesPerHour);
-    }
-
-
-    public static double sum(double firstNumber, double secondNumber) {
-        double result = firstNumber + secondNumber;
-        return result;
-    }
-
-    public static double subtract(double firstNumber, double secondNumber) {
-        double result = firstNumber - secondNumber;
-        return result;
-
-    }
-
-    public static double multiply(double firstNumber, double secondNumber) {
-        double result = firstNumber * secondNumber;
-        return result;
-
-    }
-
-    public static double divide(double firstNumber, double secondNumber) {
-        double result = firstNumber / secondNumber;
-        return result;
-
     }
 
 
